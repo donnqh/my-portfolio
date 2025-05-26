35a06ea22c0b791ba3215a0661ac5be8f9e18ebe
Don <donnqh@gmail.com>
2025-05-26 22:13:59 +0800

    Updated project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,6 +14,14 @@ function Projects() {
         <p>Weather App using OpenWeatherMap API</p>
       </div>
       <div className="project-card">
+        <h3>donRyde</h3>
+          <p>A map-based ride booking application built with react and google API</p>
+          <p>Features :
+              - Interactive map with live traffic
+              - Distance, duration and fare estimation using real-time traffic
+          </p>
+      </div>
+      <div className="project-card">
         <h3>Iventory System</h3>
           <p>Inventory system</p>
       </div>
